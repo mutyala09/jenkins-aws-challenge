52aa08414e4550cef03e7e66bb1f4c6e4e463dd4
mutyala09 <79491003+mutyala09@users.noreply.github.com>
2021-06-27 00:56:58 +0530

    Update main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -88,6 +88,14 @@ resource "aws_instance" "jenkins" {
       "echo iptables-persistent iptables-persistent/autosave_v6 boolean true | sudo debconf-set-selections",
       "sudo apt-get -y install iptables-persistent",
       "sudo ufw allow 8080",
+      "sudo apt install -y apt-transport-https ca-certificates curl software-properties-common",
+      "curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -",
+      "sudo add-apt-repository \"deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable\"",
+      "sudo apt update",
+      "sudo apt install -y docker-ce",
+      "sudo usermod -aG docker ubuntu",
+      "sudo usermod -aG docker jenkins",
+      "newgrp docker"
     ]
   }
 
